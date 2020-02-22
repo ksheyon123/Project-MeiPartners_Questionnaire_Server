2808258bb7ede7eeaed9294c9c62911f55e9e708
ksheyon123 <kshyeon123@gmail.com>
2020-02-22 16:15:51 +0900

    UserLogin & UserRegister Updated

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,10 +6,6 @@ var userModel = require('../models/userModel');
 userRouter.post('/api/register', async (req, res) => {
     try {
         console.log('register', req.body)
-
-        if(req.body.password != req.body.passwordConfirm) {
-            res.send(false)
-        }
         var result = await userModel.UserRegister(req);
         res.status(200).send(result);
     } catch (err) {
@@ -20,12 +16,8 @@ userRouter.post('/api/register', async (req, res) => {
 
 userRouter.post('/api/login', async (req, res) => {
     try {
-        console.log(req.body);
         var result = await userModel.UserLogin(req);
-        console.log(result);
-        if(result.user) {
             res.status(200).send(result);
-        }
     } catch (err) {
         console.log(err);
         res.status(500).send(err);
